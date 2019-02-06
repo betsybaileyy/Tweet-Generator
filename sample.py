d8e7973fe40616c779514c0fc5d26fb43856c146
betsybaileyy <betsy.bailey80@gmail.com>
2019-02-06 15:13:35 -0800

    refactored code into a function and gave a shoutout to Justin for helping me

--- sample.py
+++ sample.py
@@ -3,12 +3,14 @@ import random
 
 histogram = dictionary('hello world good day day day')
 
-keys = histogram.keys()
+'''
+Justin helped me map out and understand how to create this get_random_word function. Thank you, Justin!!!
+'''
+def get_random_word(histogram):
+    keys = histogram.keys()
+    word_list = list(keys)
+    random_index = random.randint(0, len(keys)-1)
+    random_word = word_list[random_index]
+    print(random_word)
 
-word_list = list(keys)
-
-random_index = random.randint(0, len(keys)-1)
-
-random_word = word_list[random_index]
-
-print(random_word)
+get_random_word(histogram)
