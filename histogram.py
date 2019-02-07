c38320e66021f449fc2843e17f23dead6ed0a36f
betsybaileyy <betsy.bailey80@gmail.com>
2019-02-06 20:37:42 -0800

    refactored code and deleted unneeded code

--- histogram.py
+++ histogram.py
@@ -1,16 +1,13 @@
 
-seuss_txt = "one fish two fish red fish blue fish red two"
 
 '''
 TODO: Update this so it takes in an external text file and doesn't only use Dr.Seuss material
 '''
 
 # As a LIST OF LISTS
-
-def word_frequency(seuss_txt):
-    words_list = seuss_txt.split(" ")
+def word_frequency(world_list):
     new_list = []
-    for word in words_list:
+    for word in word_list:
         word_found = False
         if len(new_list) == 0:
             new_list.append([word, 1])
@@ -21,17 +18,16 @@ def word_frequency(seuss_txt):
                     this_word[1] += 1
                 if not word_found:
                     new_list.append([word, 1])
-    print(new_list)
+    # print(new_list)
     return new_list
 
 # As a DICTIONARY
 
-def dictionary(seuss_txt):
-    words_list = seuss_txt.split()
+def dictionary(word_list):
     table = dict()
-    for i in words_list:
+    for i in word_list:
         table[i] = table.get(i, 0) + 1
-    print(table)
+    # print(table)
     return table
 
 # TO DO: refactor this code so that when it prints to the console it only does so once.
@@ -39,9 +35,9 @@ def dictionary(seuss_txt):
 '''
 Phyllis helped me with this function and my understanding of it SO MUCH!! Thank you, Phyllis!
 '''
-word_list = [word for word in seuss_txt.split()]
+# word_list = [word for word in seuss_txt.split()]
 
-def tuple_o_gram(word_list, text):
+def tuple_o_gram(word_list):
     count = 0
     list_of_tpls = []
 
@@ -59,19 +55,23 @@ def tuple_o_gram(word_list, text):
                     in_list = True
                     count = tpl[1] + 1
                     list_of_tpls.remove(tpl)
-                    print(list_of_tpls)
+                    # print(list_of_tpls)
                     list_of_tpls.append((word, count))
-                    print(list_of_tpls)
+                    # print(list_of_tpls)
 
                 # if was not found
             if not in_list:
                 list_of_tpls.append((word, 1))
                 # in_list = False
+    return list_of_tpls
 
 if __name__ == "__main__":
-    word_frequency(seuss_txt)
-    dictionary(seuss_txt)
-    print(tuple_o_gram(word_list, seuss_txt))
+    seuss_txt = "one fish two fish red fish blue fish"
+    word_list = seuss_txt.split()
+    # print(word_frequency(word_list))
+    # print(dictionary(word_list))
+    # print(tuple_o_gram(word_list))
+
 
 '''
 Does these problems still need to be done? (problem is below)
